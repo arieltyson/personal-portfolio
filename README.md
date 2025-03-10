5a5c79634348e41a6c11e0b856b1fca93f93a1cf
Ariel Tyson <ariel30190@hotmail.com>
2025-03-10 02:00:55 -0700

    Revert "Deploy production build to root"
    This reverts commit 6224d5032765ea76f470c864cc00a985138f5579.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Personal Website Portfolio 🍄‍🟫
+
+<kbd>
+    <img src="https://github.com/user-attachments/assets/1bdea888-5b25-4a6b-8cbd-07113cae6977" alt="tourismBeatsLogo" width="1000" height="800">
+</kbd>
+
+## Project Description 🎨
+
+This project acts as a deep dive into my professional career experience timeline, highlighting projects that I am proud of, work experience that I have garnered along the way, as well as testimonials from people I have collaborated with on this beautiful journey.
+
+
+## Technologies Used  💻
+
+This project is crafted using the harmonious blend of HTML and CSS. These technologies are the backbone of the web, and they have been meticulously wielded to create an immersive and visually pleasing experience. As a work in progress, this application will be improved and maintained and documented the entire way through detailed commit messages to allow other developers to follow along.
+
+- [x] HTML
+- [x] CSS
+- [x] React.js
+- [x] ThreeJS
+
+## Skills Demonstrated 🥋
+
+This project provided a basis for understanding the operations required for effective user experience in a functional web page. The following skills were demonstrated:
+
+- **LAYOUT**: The usage of grids to place items wherever the developer likes.
+
+- **USER EXPERIENCE**: A clean landscape promoting visibility, visual appeal and ease of use.
+
+- **USER INTERFACE**: An interactive interface allowing easy and effective navigation.
+
+- **WEB DESIGN**: The brush of creativity to add a unique look to a functional tool.
+
+
+## Contributing ⚙️
+
+We believe that great knowledge like that which you possess is meant to be shared, and we welcome contributions from fellow geniuses and developers alike. If you have a beneficial or creative code snippet to add or a design improvement to suggest, feel free to fork our repository, make your enhancements, and submit a pull request. Together, let's make the world even more advanced.
+
+## License 🪪
+
+This project is licensed under the [MIT License](LICENSE), which means you're free to modify, distribute, and use the code as you see fit. Just remember to attribute the original work to **Ariel Tyson's GitHub** and keep the aspiring developer spirit alive!
+
+---
---
+++ src/assets/index.js
@@ -0,0 +1,112 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import azure from "./tech/azure.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import rust from "./tech/rust.png";
+import ruby from "./tech/ruby.png";
+import rails from "./tech/rails.png";
+import python from "./tech/python.png";
+import native from "./tech/native.png";
+import java from "./tech/java.png";
+import cplusplus from "./tech/cplusplus.png";
+import c from "./tech/c.png";
+import bash from "./tech/bash.png";
+import csharp from "./tech/csharp.png"
+import swift from "./tech/swift.png"
+
+import CJSF from "./company/CJSF.png";
+import tutor from "./company/tutor.png";
+import mt from "./company/mt.png";
+import am from "./company/am.png";
+import bl from "./company/bl.png";
+import bi from "./company/bi.png";
+import rad from "./company/rad.png";
+import rs from "./company/rs.png";
+import workday from "./company/workday.png";
+import twitch from "./company/twitch.png";
+import teals from "./company/teals.png";
+import microsoft_teals from "./company/microsoft_teals.png";
+
+import puzzle from "./puzzle.png";
+import quote from "./quote.png";
+import rps from "./rps.png";
+import ios1 from "./ios1.png";
+import ios2 from "./ios2.png";
+import ios3 from "./ios3.png";
+import list from "./list.png";
+import fish from "./fish.png";
+import mortgage from "./mortgage.png";
+import event from "./event.jpg.png";
+import trivia from "./trivia.png";
+import contacts from "./contacts.png";
+import expenses from "./expenses.png";
+import checklist from "./checklist.png";
+import processor from "./processor.png";
+import beats from "./beats.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  azure,
+  html,
+  javascript,
+  reactjs,
+  CJSF,
+  tutor,
+  rad,
+  mt,
+  am,
+  bl,
+  bi,
+  puzzle,
+  quote,
+  rps,
+  rust,
+  ruby,
+  rails,
+  python,
+  native,
+  java,
+  cplusplus,
+  c,
+  csharp,
+  swift,
+  bash,
+  ios1,
+  ios2,
+  ios3,
+  list,
+  fish,
+  mortgage,
+  event,
+  trivia,
+  contacts,
+  expenses,
+  checklist,
+  processor,
+  rs,
+  workday,
+  twitch,
+  teals,
+  beats,
+  microsoft_teals
+};
